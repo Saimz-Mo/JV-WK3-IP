3d438d74bd1d2d5f24843e3f2529aba3e12a78f5
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-11-10 19:33:03 +0300

    add override methods for ThrivingAnimalDao

--- src/main/java/ke/co/safaricom/models/ThrivingAnimalDao.java
+++ src/main/java/ke/co/safaricom/models/ThrivingAnimalDao.java
@@ -14,4 +14,59 @@ public class ThrivingAnimalDao implements ThrivingAnimalManagement {
         }
 
     }
+
+    @Override
+    public void add(ThrivingAnimal thrivingAnimal) {
+        getDrivers();
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (animalName, animalType) VALUES (:animalName, :animalType)";
+
+            thrivingAnimal.id = (int) con.createQuery(sql, true)
+                    .addParameter("animalName", thrivingAnimal.animalName)
+                    .addParameter("animalType", thrivingAnimal.animalType)
+                    .executeUpdate()
+                    .getKey();
+        } catch (Sql2oException e){
+            System.out.println(e);
+        }
+
+    }
+
+    @Override
+    public void update(ThrivingAnimal thrivingAnimal) {
+
+    }
+
+    @Override
+    public ThrivingAnimal getThrivingAnimalById(int id) {
+        getDrivers();
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery("SELECT * FROM animals WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(ThrivingAnimal.class);
+        }
+
+    }
+
+    @Override
+    public List<ThrivingAnimal> getThrivingAnimals() {
+        getDrivers();
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery(" SELECT * FROM animals")
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(ThrivingAnimal.class);
+
+        }
+
+    }
+
+    @Override
+    public void deleteThrivingAnimal(int id) {
+
+    }
+
+    @Override
+    public void deleteAllThrivingAnimals() {
+
+    }
 }
