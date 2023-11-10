da51edcab305ae150dab3853a8db0b01cfaa7828
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-11-10 18:41:34 +0300

    override Sightings

--- src/main/java/ke/co/safaricom/models/Sightings.java
+++ src/main/java/ke/co/safaricom/models/Sightings.java
@@ -1,5 +1,7 @@
 package ke.co.safaricom.models;
 
+import java.util.List;
+
 public class Sightings implements SightingsManagement{
     private int animalId;
     private int id;
@@ -63,5 +65,83 @@ public class Sightings implements SightingsManagement{
 
     public void setTimestamp(Timestamp timestamp) {
         this.timestamp = timestamp;
+    }
+
+    @Override
+    public void save() {
+        String sql = "INSERT INTO sightings (animalId, location, ranger, timestamp) VALUES (:animalId, :location, :ranger, now());";
+        System.out.println("INSERT INTO sightings (animalId, location, ranger, timestamp) VALUES (:animalId, :location, :ranger, now());");
+        try (Connection con = DB.sql2o.open()) {
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("animalId", this.animalId)
+                    .addParameter("location", this.location)
+                    .addParameter("ranger", this.ranger)
+                    .executeUpdate()
+                    .getKey();
+        }
+
+    }
+
+    public static List<Sightings> all() {
+        String sql = "SELECT * FROM sightings ORDER BY timestamp DESC;";
+
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Sightings.class);
+        }
+    }
+
+    public static List<Sightings> allByAnimal(int animalId) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM sightings WHERE animalId = :animalId ORDER BY timestamp DESC";
+            return con.createQuery(sql)
+                    .addParameter("animalId", animalId)
+                    .executeAndFetch(Sightings.class);
+        }
+    }
+
+    public boolean equals(Object otherSighting){
+        if(!(otherSighting instanceof Sightings)){
+            return false;
+        }else{
+            Sightings newSighting = (Sightings) otherSighting;
+            return this.getAnimalId()==newSighting.getAnimalId() && this.getRanger().equals(newSighting.getRanger());
+        }
+    }
+
+    public static Sightings find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM sightings WHERE id=:id;";
+            Sightings sighting = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Sightings.class);
+            return sighting;
+        } catch (IndexOutOfBoundsException exception) {
+            return null;
+        }
+    }
+    @Override
+    public void delete() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM sightings WHERE id=:id;";
+            con.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeUpdate();
+        }
+
+    }
+
+    public void update() {
+        String sql = "UPDATE sightings SET location = :location, ranger = :ranger WHERE id = :id";
+
+        try(Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("location", location)
+                    .addParameter("ranger", ranger)
+                    .addParameter("id", id)
+                    .throwOnMappingFailure(false)
+                    .executeUpdate();
+        }
     }
 }
