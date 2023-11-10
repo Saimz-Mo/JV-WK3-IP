ea03eebf1fe917b4a23d4c9ce61e0d045a358e2f
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-11-10 18:50:52 +0300

    add interface ThrivingAnimalManagement

---
+++ src/main/java/ke/co/safaricom/models/ThrivingAnimalManagement.java
@@ -0,0 +1,13 @@
+package ke.co.safaricom.models;
+
+import java.util.List;
+
+public interface ThrivingAnimalManagement {
+    
+    void add(ThrivingAnimal thrivingAnimal);
+    void update(ThrivingAnimal thrivingAnimal);
+    ThrivingAnimal getThrivingAnimalById(int id);
+    List<ThrivingAnimal> getThrivingAnimals();
+    void deleteThrivingAnimal(int id);
+    void deleteAllThrivingAnimals();
+}
