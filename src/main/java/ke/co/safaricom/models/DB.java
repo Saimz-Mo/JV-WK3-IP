177f8830c8d41254949faec42818d80d80c67bde
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-11-11 10:47:34 +0300

    add class EndangeredAnimalsTest

--- src/main/java/ke/co/safaricom/models/DB.java
+++ src/main/java/ke/co/safaricom/models/DB.java
@@ -19,5 +19,5 @@ public class DB {
     //        "jdbc:postgresql://ec2-54-158-247-210.compute-1.amazonaws.com:5432/dek5veegvsbu4",
     //        "zqcqcwnxeihpsu",
     //        "7cb2962b3c8ad1ab91e7923204bd4d9646887af835f755c3a0964e12d372ca1d"
-    );
+    //);
 }
